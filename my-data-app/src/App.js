dbe4e25bb57f7824ef84d681b7724393876d0f0c
Alice <alicemilea04@gmail.com>
2025-06-29 20:09:12 -0400

    added frontend

---
+++ my-data-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello
+        </h1>
+      </header>
+
+      <div className='App-body'>
+        <p>Here be me div</p>
+      </div>
+
+      <footer className='App-foot'>
+        <p>here's me foot</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
