6b604a53005b403b83419b9d1bfb7fb94b82ac8c
Alice <alicemilea04@gmail.com>
2025-07-06 11:06:51 -0400

    put parser on cards

--- my-data-app/src/FileParser.js
+++ my-data-app/src/FileParser.js
@@ -3,14 +3,14 @@ import { useCSVReader, formatFileSize } from "react-papaparse";
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 import { Pie } from 'react-chartjs-2';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 Chart.register(CategoryScale);
 
 export default function Reader() {
     const { CSVReader } = useCSVReader();
-
-    const [col, setCol] = useState([]);
-    const [val, setVal] = useState([]);
+    const [col, setCol] = useState([]); // headers
+    const [val, setVal] = useState([]); // values
 
     return (
         <CSVReader
@@ -30,53 +30,72 @@ export default function Reader() {
                 getRemoveFileProps,
                 Remove,
             }) => (
-                <>
-                    <div {...getRootProps()}>
-                        {acceptedFile ? (
-                            <>
-                                <div className="info-container">
-                                    <div>
-                                        <p>{acceptedFile.name}</p>
-                                        <span>{formatFileSize(acceptedFile.size)}</span>
+                <> <Container>
+                    <Row>
+                        <Col md={6} className="mx-auto">  {/* 4 out of 12 columns = 1/3 */}
+                            <Card className='TheCard'>
+                                <Card.Body>
+                                    <div {...getRootProps()}>
+                                        {acceptedFile ? (
+                                            <>
+                                                <div className="info-container">
+                                                    <div>
+                                                        <p>{acceptedFile.name}</p>
+                                                        <span>{formatFileSize(acceptedFile.size)}</span>
+                                                    </div>
+                                                    <div className="info__progress">
+                                                        <ProgressBar />
+                                                    </div>
+                                                    <div {...getRemoveFileProps()} className="info__remove">
+                                                        <Remove color={"red"} />
+                                                    </div>
+                                                </div>
+                                            </>
+                                        ) : (
+                                            <button>Upload file</button>
+                                        )}
                                     </div>
-                                    <div className="info__progress">
-                                        <ProgressBar />
-                                    </div>
-                                    <div {...getRemoveFileProps()} className="info__remove">
-                                        <Remove color={"red"} />
-                                    </div>
-                                </div>
-                            </>
-                        ) : (
-                            <button>Upload file</button>
-                        )}
-                    </div>
-                    <div className="App">
-                        {col.length > 0 && val.length > 0 && (
-                            <Pie
-                                data={{
-                                    labels: col,
-                                    datasets: [
-                                        {
-                                            label: 'My Data',
-                                            data: val.map(row => parseFloat(row[1])),
-                                            backgroundColor: [
-                                                '#FF6384',
-                                                '#36A2EB',
-                                                '#FFCE56',
-                                                '#4BC0C0',
-                                                '#9966FF',
-                                                '#FF9F40'
-                                            ],
-                                        },
-                                    ],
-                                }}
-                            />
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={6} className="mx-auto">  {/* 4 out of 12 columns = 1/3 */}
 
-                        )}
-                    </div>
+                            <Card className='TheCard'>
+                                <Card.Body>
+                                    <div className="App">
+                                        {col.length > 0 && val.length > 0 && (
+                                            <Pie
+                                                data={{
+                                                    labels: col,
+                                                    datasets: [
+                                                        {
+                                                            label: 'My Data',
+                                                            data: val.map(row => parseFloat(row[1])),
+                                                            backgroundColor: [
+                                                                '#FF6384',
+                                                                '#36A2EB',
+                                                                '#FFCE56',
+                                                                '#4BC0C0',
+                                                                '#9966FF',
+                                                                '#FF9F40'
+                                                            ],
+                                                        },
+                                                    ],
+                                                }}
+                                            />
+
+                                        )}
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
                 </>
-            )}
-        </CSVReader>
+            )
+            }
+        </CSVReader >
     );
 }
